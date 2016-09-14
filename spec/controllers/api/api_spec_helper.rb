4954690916af096c7d8091daf34ce9220de4b851
Csaba Nemeth <csaba.nemeth@csabanemeth.org>
2016-09-14 12:44:51 -0700

    Fixed api spec helper so regular controller specs won't bork

--- spec/controllers/api/api_spec_helper.rb
+++ spec/controllers/api/api_spec_helper.rb
@@ -5,7 +5,7 @@ require 'rack/test'
 Dir[File.expand_path(File.join(File.dirname(__FILE__), 'api', '**', '*.rb'))].each { |f| require f }
 
 RSpec.configure do |config|
-  config.include Rack::Test::Methods
+  config.include Rack::Test::Methods, type: :request
   config.include RSpec::Rails::RequestExampleGroup, type: :request, file_path: File.join('app', 'controllers', 'api', '**', '*.rb')
 end
 
