30766690e22c59f7daec9f00355e024659efe92a
Csaba Nemeth <csaba.nemeth@csabanemeth.org>
2016-09-13 21:43:32 -0700

    Return tags for message in revese order

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,6 +1,6 @@
 module MessagesHelper
   def tags_for(message)
-    message.tags.sort{|a,b| a.created_at <=> b.created_at}.map(&:tag)
+    message.tags.sort{|a,b| a.created_at <=> b.created_at}.map(&:tag).reverse
   end
 end
 
