c9620c91b6cd5299346e8e1877059765e5d62404
Csaba Nemeth <csaba.nemeth@csabanemeth.org>
2016-09-13 20:38:01 -0700

    Added non-api specific configuration

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,15 @@
 require_relative 'boot'
 
-require "rails"
+#require "rails"
+require 'rails/all'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "action_cable/engine"
+#require "active_model/railtie"
+#require "active_job/railtie"
+#require "active_record/railtie"
+#require "action_controller/railtie"
+#require "action_mailer/railtie"
+#require "action_view/railtie"
+#require "action_cable/engine"
 # require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
@@ -25,7 +26,7 @@ module Zozi
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+    #config.api_only = true
 
     config.generators do |g|
       g.test_framework  :rspec, fixture: true
