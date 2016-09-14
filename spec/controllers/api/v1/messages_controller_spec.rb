6bcc6ad7bb15cf79609ac24297af6b7b2c33272a
Csaba Nemeth <csaba.nemeth@csabanemeth.org>
2016-09-14 08:18:20 -0700

    Added spec for GET '/api/v1/messages'

--- spec/controllers/api/v1/messages_controller_spec.rb
+++ spec/controllers/api/v1/messages_controller_spec.rb
@@ -1,4 +1,15 @@
-require 'rails_helper'
+require_relative '../../api/api_spec_helper'
 
-RSpec.describe Api::V1::MessagesController do
+RSpec.describe Api::V1::MessagesController, type: :request do
+  describe "GET '/api/v1/messages'" do
+    it 'sends a list of messages' do
+      FactoryGirl.create_list(:message, 10)
+
+      get '/api/v1/messages'
+
+      json = JSON.parse(last_response.body)
+      expect(last_response.status).to eq 200
+      expect(json.count).to eq 10
+    end
+  end
 end
